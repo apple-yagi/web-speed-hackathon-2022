aaaaf4fdaaded678346011fcc65950d673db3145
apple-yagi <applesryu@gmail.com>
2022-11-06 22:03:23 +0900

    Use dom.watch

--- src/client/foundation/side-effects.js
+++ src/client/foundation/side-effects.js
@@ -2,4 +2,4 @@ import { dom, library } from "@fortawesome/fontawesome-svg-core";
 import { faInfoCircle, faTicketAlt } from "@fortawesome/free-solid-svg-icons";
 import { faHandPeace } from "@fortawesome/free-regular-svg-icons";
 library.add(faInfoCircle, faTicketAlt, faHandPeace);
-dom.i2svg();
+dom.watch();
