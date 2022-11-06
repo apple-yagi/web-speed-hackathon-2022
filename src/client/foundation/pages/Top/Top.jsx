bab6093dad76005d47c770a6f7d4ee65b8914f1a
apple-yagi <applesryu@gmail.com>
2022-11-06 17:37:55 +0900

    Remove useHeroImage

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -77,26 +77,6 @@ function useTodayRacesWithAnimation(races) {
   return racesToShow;
 }
 
-/**
- * @param {Model.Race[]} todayRaces
- * @returns {string | null}
- */
-function useHeroImage(todayRaces) {
-  const firstRaceId = todayRaces[0]?.id;
-  const url =
-    firstRaceId !== undefined
-      ? `/api/hero?firstRaceId=${firstRaceId}`
-      : "/api/hero";
-  const { data } = useFetch(url, jsonFetcher);
-
-  if (firstRaceId === undefined || data === null) {
-    return null;
-  }
-
-  const imageUrl = `${data.url}?${data.hash}`;
-  return imageUrl;
-}
-
 /** @type {React.VFC} */
 export const Top = () => {
   const { date = moment().format("YYYY-MM-DD") } = useParams();
@@ -145,11 +125,9 @@ export const Top = () => {
           )
       : [];
   const todayRacesToShow = useTodayRacesWithAnimation(todayRaces);
-  // const heroImageUrl = useHeroImage(todayRaces);
 
   return (
     <Container>
-      {/* {heroImageUrl !== null && <HeroImage url={heroImageUrl} />} */}
       <HeroImage url="/assets/images/hero.jpg" />
 
       <Spacer mt={Space * 2} />
